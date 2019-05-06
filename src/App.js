73a549d90e932c06aaf81f8a03b9ae81f07d0e00
Rachel Oh <rachelyjo@gmail.com>
2019-05-06 13:14:30 -0700

    remove comments

--- src/App.js
+++ src/App.js
@@ -32,16 +32,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-// The App component should render markup matching the design.html: the main element, a header with the h1 and a div with class 'App-list'.
-// The App component will accept 1 prop, store.
-    // The store prop is an object with 2 keys: lists and allCards.
-    // lists is an array of objects.
-    // allCards is an object where each key is a card's ID and the value is the card object with a title and content.
-    // You'll be given a dummy STORE object to pass to your App.
-// The App should render a List component for each of the items in the store.lists array.
-// Each instance of the List component should be passed 2 props (and a key). The 2 props are header and cards.
-    // The header prop is a string for the List's header.
-    // The cards prop will be an array of card objects.
-      // You'll need to combine the cardIds array for the list with the allCards object.
